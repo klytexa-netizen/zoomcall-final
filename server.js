7df067484a94190c1c584626a90c3d1e85a4720c
klytexa-netizen <klytexa@gmail.com>
2026-04-18 12:31:39 +0100

    Fixed Zoom Call - working video chat

--- server.js
+++ server.js
@@ -6,42 +6,58 @@ const io = require('socket.io')(server);
 app.use(express.static('public'));
 app.use(express.json());
 
-// Simple login
-app.post('/login', (req, res) => {
-    const { email, name } = req.body;
-    res.json({ success: true, name: name || email.split('@')[0] });
-});
-
-// Online users
+// Store online users
 let onlineUsers = [];
 
 io.on('connection', (socket) => {
     console.log('User connected:', socket.id);
     
-    socket.on('join', (user) => {
-        onlineUsers.push({ id: socket.id, name: user.name, email: user.email });
-        io.emit('users', onlineUsers);
+    socket.on('user-join', (userData) => {
+        onlineUsers.push({
+            socketId: socket.id,
+            name: userData.name,
+            email: userData.email
+        });
+        io.emit('update-users', onlineUsers);
+        console.log('User joined:', userData.name);
     });
     
-    socket.on('offer', (data) => {
-        socket.to(data.target).emit('offer', { offer: data.offer, from: socket.id, fromName: data.fromName });
+    socket.on('call-user', (data) => {
+        console.log('Call from', socket.id, 'to', data.to);
+        io.to(data.to).emit('incoming-call', {
+            from: socket.id,
+            fromName: data.fromName,
+            offer: data.offer
+        });
     });
     
-    socket.on('answer', (data) => {
-        socket.to(data.target).emit('answer', { answer: data.answer, from: socket.id });
+    socket.on('answer-call', (data) => {
+        console.log('Answer from', socket.id, 'to', data.to);
+        io.to(data.to).emit('call-answered', {
+            answer: data.answer,
+            from: socket.id
+        });
     });
     
-    socket.on('ice', (data) => {
-        socket.to(data.target).emit('ice', { candidate: data.candidate, from: socket.id });
+    socket.on('ice-candidate', (data) => {
+        io.to(data.to).emit('ice-candidate', {
+            candidate: data.candidate,
+            from: socket.id
+        });
+    });
+    
+    socket.on('end-call', (data) => {
+        io.to(data.to).emit('call-ended');
     });
     
     socket.on('disconnect', () => {
-        onlineUsers = onlineUsers.filter(u => u.id !== socket.id);
-        io.emit('users', onlineUsers);
+        onlineUsers = onlineUsers.filter(u => u.socketId !== socket.id);
+        io.emit('update-users', onlineUsers);
+        console.log('User disconnected:', socket.id);
     });
 });
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`✅ Server running on port ${PORT}`);
 });
